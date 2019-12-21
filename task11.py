df769cd7c44f1f1c4f0b857eafcb0175ad177923
baha312 <bahtiyarbolotbekov@gmail.com>
2019-12-21 11:51:21 +0600

    first commit

---
+++ task11.py
@@ -0,0 +1,6 @@
+# Given a string consisting of words separated by spaces. Determine how many
+# words it has. To solve the problem, use the method count.
+
+str = "London is capital city of Great Britain"
+new_x = (str.count(" "))
+print(new_x+1)
